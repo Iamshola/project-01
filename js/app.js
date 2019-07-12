6a9b4f69e99c267897ac504743c4c0d24daaf6be
Adesola Oni-Shogbonyo <s.oni-shogbonyo@hotmail.co.uk>
2019-07-12 13:02:20 +0100

    Grid scoped out

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log('hey there')
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div')
+  const obstacles = document.querySelector('.obstacles')
+
+
+  // Move moveMyCharacter
+  let currentIndex = 30
+  const width = 6
+
+  function moveMyCharacter(e) {
+    squares[currentIndex].classList.remove('mole')
+
+    switch(e.keyCode) {
+      case 37:
+        if(currentIndex % width !== 0) currentIndex -= 1
+        break
+      case 38:
+        if(currentIndex - width >= 0) currentIndex -= width
+        break
+      case 39:
+        if(currentIndex % width < width - 1) currentIndex += 1
+        break
+      case 40:
+        if(currentIndex + width < width * width) currentIndex += width
+        break
+    }
+
+    if(squares[currentIndex].classList.contains('mole')) {
+      squares[currentIndex].classList.remove('mole')
+    }
+    squares[currentIndex].classList.add('mole')
+  }
+
+  document.addEventListener('keyup', moveMyCharacter)
+
+  // function moveObstacles(){
+  //
+  // }
+
+
+  // Need Braces
+
+})
