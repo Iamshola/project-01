d1c0a031a056d39126b9a57269d66c775486141e
Adesola Oni-Shogbonyo <s.oni-shogbonyo@hotmail.co.uk>
2019-07-13 13:36:49 +0100

    reformatted code for cars

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squares = document.querySelectorAll('.grid div')
   const obstacles = document.querySelector('.obstacles')
+  const riskArea = document.querySelectorAll('.riskArea')
+  const score = document.querySelector('score')
 
   // Move moveMyCharacter starts
   let currentIndex = 30
@@ -34,18 +36,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // Move moveMyCharacter ends
 
   // goObstaclesLogs starts
-  // let currentIndex1 = 6
-  // function goObstaclesLogs(){
-  //   if(currentIndex1 === 12){
-  //     squares[currentIndex1 - width].classList.add('obstacles')
-  //     currentIndex1 = 6
-  //   } else if (currentIndex1 >= 6){
-  //     squares[currentIndex1].classList.add('obstacles')
-  //     currentIndex1++
-  //     setTimeout(() => squares[currentIndex1-1].classList.remove('obstacles'), 750)
-  //   }
-  // }
-  // setInterval(goObstaclesLogs, 500)
+  let currentIndex1 = 6
+  function goObstaclesLogs(){
+    if(currentIndex1 === 12){
+      squares[currentIndex1 - width].classList.add('obstacles')
+      currentIndex1 = 6
+    } else if (currentIndex1 >= 6){
+      squares[currentIndex1].classList.add('obstacles')
+      currentIndex1++
+      setTimeout(() => squares[currentIndex1-1].classList.remove('obstacles'), 750)
+    }
+  }
+  setInterval(goObstaclesLogs, 500)
   // goObstaclesLogs ends
 
   //goObstaclesLog2 starts
@@ -64,23 +66,33 @@ document.addEventListener('DOMContentLoaded', () => {
   //goObstaclesLog2 ends
 
   //goObstaclesThree starts
-  // let currentIndex3 = 12
-  // function goObstaclesThree(){
-  //   if(currentIndex3 === 18){
-  //     squares[currentIndex3 - width].classList.add('obstacles')
-  //     currentIndex3 = 12
-  //   } else if (currentIndex3 >= 12){
-  //     squares[currentIndex3].classList.add('obstacles')
-  //     currentIndex3++
-  //     setTimeout(() => squares[currentIndex3-1].classList.remove('obstacles'), 750)
-  //   }
-  // }
-  // setInterval(goObstaclesThree, 1000)
+  let currentIndex3 = 24
+  function goObstaclesThree(){
+    if(currentIndex3 === 30){
+      squares[currentIndex3 - width].classList.add('obstacles')
+      currentIndex3 = 24
+    } else if (currentIndex3 >= 24){
+      squares[currentIndex3].classList.add('obstacles')
+      currentIndex3++
+      setTimeout(() => squares[currentIndex3-1].classList.remove('obstacles'), 750)
+    }
+  }
+  setInterval(goObstaclesThree, 1000)
   //goObstaclesTwo ends
 
+  // MakeGoals starts
+  function getGoals(){
+    const randomIndex = Math.floor(Math.random() * riskArea.length)
+    const randomriskArea = riskArea[randomIndex]
+    riskArea[randomIndex].classList.add('gem')
+    if(squares[currentIndex].classList.contains('gem')) {
+      squares[currentIndex].classList.remove('mole')
+    }
+    squares[currentIndex].classList.add('mole')
+  }
 
-
-
+  getGoals()
+  // MakeGoals ends
 
 
 
